144b98f3fee49d48fd90be411ddccfff22499b87
bugyaluwang <jiabowang@moego.pet>
2024-10-17 09:59:51 +0800

    build: v1.0.6 - delete postinstall

--- scripts/dpdm.js
+++ scripts/dpdm.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
+#!/usr/bin/env node
+
 const path = require('path');
 const os = require('os');
+const { spawn } = require('child_process');
 
 const platform = os.platform();
 const arch = os.arch();
@@ -9,38 +11,27 @@ const key = `${platform}-${arch}`;
 
 const keyStore = {
   'darwin-arm64': 'aarch64-apple-darwin',
-  // 'darwin-x64': 'x86_64-apple-darwin',
+  'darwin-x64': 'x86_64-apple-darwin',
   'linux-arm64': 'aarch64-unknown-linux-musl',
   'linux-x64': 'x86_64-unknown-linux-musl',
   'win32-x64': 'x86_64-pc-windows-gnu',
-  'win32-arm64': 'aarch64-pc-windows-gnu',
 };
 
-const binName = platform === 'win32' ? 'dpdm.exe' : 'dpdm';
-
 const sourceDir = path.join(__dirname, '../target', keyStore[key], 'release');
-const sourceFile = path.join(sourceDir, binName);
+const binFile = path.join(
+  sourceDir,
+  platform === 'win32' ? 'dpdm.exe' : 'dpdm',
+);
 
-const targetDir = path.join(__dirname, '../target', 'release');
-const targetFile = path.join(targetDir, binName);
+const args = process.argv.slice(2);
 
-if (!fs.existsSync(targetDir)) {
-  fs.mkdirSync(targetDir, { recursive: true });
-}
-fs.copyFile(sourceFile, targetFile, (err) => {
-  if (err) {
-    console.error('Copy failed:', err);
-  }
+const child = spawn(binFile, args, { stdio: 'inherit' });
+
+child.on('close', (code) => {
+  process.exit(code);
 });
 
-if (platform === 'win32') {
-  const packageJson = path.join(__dirname, '../package.json');
-  const packageJsonContent = fs.readFileSync(packageJson, 'utf8');
-  const packageJsonObj = JSON.parse(packageJsonContent);
-  packageJsonObj.bin = binName;
-  fs.writeFileSync(
-    packageJson,
-    JSON.stringify(packageJsonObj, null, 2),
-    'utf8',
-  );
-}
+child.on('error', (error) => {
+  console.error(`Failed to execute ${binFile}: ${error.message}`);
+  process.exit(error.code);
+});
